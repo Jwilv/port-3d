15152988134504892d7c9f0c1fe43db1b2432f80
Jwilv <juan.ignacio.wilvers@gmail.com>
2023-11-04 18:19:00 -0300

    logo

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import Head from 'next/head'
 import {
   About, Contact, Experience,
   Feedbacks, Hero, Navbar,
@@ -8,22 +9,27 @@ import {
 
 export default function Home() {
   return (
-    <div className="relative z-0 bg-primary">
-      <div className="bg-hero-pattern bg-cover 
+    <>
+    <Head>
+      <link rel="icon" href="/logo.svg" />
+    </Head>
+      <div className="relative z-0 bg-primary">
+        <div className="bg-hero-pattern bg-cover 
       bg-no-repeat bg-center"
-      >
-        <Navbar />
-        <Hero />
+        >
+          <Navbar />
+          <Hero />
+        </div>
+        <About />
+        <Experience />
+        <Tech />
+        <Works />
+        <Feedbacks />
+        <div className="relative z-0">
+          <Contact />
+          <Stars />
+        </div>
       </div>
-      <About />
-      <Experience />
-      <Tech />
-      <Works />
-      <Feedbacks />
-      <div className="relative z-0">
-        <Contact />
-        <Stars />
-      </div>
-    </div>
+    </>
   )
 }
