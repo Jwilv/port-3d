134efa4a18fb69f70b7fb728c9a82af69f4bca12
Jwilv <juan.ignacio.wilvers@gmail.com>
2024-04-16 17:33:58 -0300

    update data

--- data/localData.ts
+++ data/localData.ts
@@ -14,71 +14,71 @@ export const Proyects: Proyetc[] = [
             },
             {
                 label: 'Linkedin',
-                url: 'https://github.com/Jwilv/Dsd_Lite'
+                url: 'https://www.linkedin.com/posts/juan-wilvers-450a11233_desarrollodesoftware-nextjs-typescript-activity-7157423038051893250-6-uB?utm_source=share&utm_medium=member_desktop'
             }
         ]
     },
     {
         title: 'spendWise',
         image: spendWise,
-        desc: 'Con llamadas, video llamadas, servidores y mensajes privados. Máxima funcionalidad, mínimo consumo de recursos. ¡Comunicación sin complicaciones!',
-        subDesc: 'Con llamadas, video llamadas, servidores y mensajes privados. Máxima funcionalidad, mínimo consumo de recursos. ¡Comunicación sin complicaciones!',
+        desc: 'spendWise. Es una aplicación móvil que te permite registrar tus gastos e ingresos de manera rápida y sencilla.',
+        subDesc: 'Lo mejor de SpendWise es que no solo te permite registrar tus transacciones, sino que también te brinda estadísticas y gráficos para que puedas entender mejor tus hábitos de gasto. ¡Es sorprendente ver en qué gastas realmente tu dinero! Una de las características de SpendWise son los retos mensuales. Puedes establecer metas de gastos para el mes',
         links: [
             {
                 label: 'Github',
-                url: 'https://github.com/Jwilv/Dsd_Lite'
+                url: 'https://github.com/Jwilv/SpendWise'
             },
             {
                 label: 'Linkedin',
-                url: 'https://github.com/Jwilv/Dsd_Lite'
+                url: 'https://www.linkedin.com/posts/juan-wilvers-450a11233_reactnative-typescript-javascript-activity-7175599838414184448-4AA6?utm_source=share&utm_medium=member_desktop'
             }
         ]
     },
     {
-        title: 'Calendar App',
+        title: 'CoCalend',
         image: calendar,
-        desc: 'Con llamadas, video llamadas, servidores y mensajes privados. Máxima funcionalidad, mínimo consumo de recursos. ¡Comunicación sin complicaciones!',
-        subDesc: 'Con llamadas, video llamadas, servidores y mensajes privados. Máxima funcionalidad, mínimo consumo de recursos. ¡Comunicación sin complicaciones!',
+        desc: 'coCalend es la herramienta perfecta para planificar eventos en equipo. Crea, comparte y coordina eventos fácilmente con amigos, familiares o colegas. Desde reuniones hasta fiestas.',
+        subDesc: 'coCalend te mantiene organizado y conectado en todo momento, sin importar si estás en tu computadora, tu móvil o tu smartwatch. Simplifica la planificación de eventos y únete a la comunidad de usuarios de coCalend hoy mismo.',
         links: [
             {
                 label: 'Github',
-                url: 'https://github.com/Jwilv/Dsd_Lite'
+                url: 'https://github.com/Jwilv/coCalend'
             }
         ]
     },
     {
-        title: 'map App',
+        title: 'LiveSpot',
         image: mapApp,
-        desc: 'Con llamadas, video llamadas, servidores y mensajes privados. Máxima funcionalidad, mínimo consumo de recursos. ¡Comunicación sin complicaciones!',
-        subDesc: 'Con llamadas, video llamadas, servidores y mensajes privados. Máxima funcionalidad, mínimo consumo de recursos. ¡Comunicación sin complicaciones!',
+        desc: 'LiveSpot te lleva más allá de los límites del mapa al proporcionarte una experiencia en tiempo real de navegación y descubrimiento compartido. Con LiveSpot, puedes marcar puntos de interés en el mapa y ver las actualizaciones en vivo de otros usuarios mientras exploran el mundo a tu alrededor.',
+        subDesc: 'Desde eventos locales hasta lugares destacados, LiveSpot te conecta con la acción en tiempo real, permitiéndote descubrir, compartir y explorar juntos.',
         links: [
             {
                 label: 'Github',
-                url: 'https://github.com/Jwilv/Dsd_Lite'
+                url: 'https://github.com/Jwilv/LiveSpot'
             }
         ]
     },
     {
-        title: 'Imgs IA',
+        title: 'AI Creators',
         image: imgsIa,
-        desc: 'Con llamadas, video llamadas, servidores y mensajes privados. Máxima funcionalidad, mínimo consumo de recursos. ¡Comunicación sin complicaciones!',
-        subDesc: 'Con llamadas, video llamadas, servidores y mensajes privados. Máxima funcionalidad, mínimo consumo de recursos. ¡Comunicación sin complicaciones!',
+        desc: 'AI Creators es tu herramienta para la expresión artística personalizada. Utilizando inteligencia artificial, transformamos tus especificaciones en iamágenes únicas y cautivadoras.',
+        subDesc: 'Después, compártelas en nuestra comunidad de artistas, donde podrás inspirarte, obtener retroalimentación y explorar el talento de otros usuarios. Únete a AI Creators y da vida a tus ideas creativas como nunca antes.',
         links: [
             {
                 label: 'Github',
-                url: 'https://github.com/Jwilv/Dsd_Lite'
+                url: 'https://github.com/Jwilv/AI-Creators'
             }
         ]
     },
     {
-        title: 'chat app',
+        title: 'GlobalChat',
         image: chatApp,
-        desc: 'Con llamadas, video llamadas, servidores y mensajes privados. Máxima funcionalidad, mínimo consumo de recursos. ¡Comunicación sin complicaciones!',
-        subDesc: 'Con llamadas, video llamadas, servidores y mensajes privados. Máxima funcionalidad, mínimo consumo de recursos. ¡Comunicación sin complicaciones!',
+        desc: 'GlobalChat te conecta instantáneamente con personas de todo el mundo en un emocionante ambiente de chat en tiempo real. Con la capacidad de ver quién está en línea en cada momento, GlobalChat te ofrece la oportunidad de entablar conversaciones con personas de diferentes culturas y lugares, en cualquier momento del día.',
+        subDesc: 'Además, con nuestro sólido sistema de reportes y baneos, puedes garantizar un entorno seguro y respetuoso para todos los usuarios. Y gracias a nuestro traductor automático integrado, la barrera del idioma desaparece, permitiéndote comunicarte sin problemas con personas de todo el mundo.',
         links: [
             {
                 label: 'Github',
-                url: 'https://github.com/Jwilv/Dsd_Lite'
+                url: 'https://github.com/Jwilv/GlobalChat'
             }
         ]
     },
