5bbe82114c53b3b6596429a83cc748c828548703
Jwilv <juan.ignacio.wilvers@gmail.com>
2024-04-16 16:51:25 -0300

    update about

--- components/screens/About.tsx
+++ components/screens/About.tsx
@@ -10,8 +10,12 @@ export const About = () => {
             <div className="md:w-1/2 w-auto mr-10 flex flex-col justify-center md:items-start items-center">
                 <div className="md:ml-[180px] ml-10 items-center justify-center flex-wrap">
                     <span className="text-xl">Sobre mi</span>
-                    <p className="font-semibold md:text-3xl text-2xl md:w-[580px] mt-10">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odit vitae nihil vero sit labore itaque soluta error totam, temporibus illum et incidunt asperiores laborum unde eligendi odio sapiente nemo. Optio.</p>
-                    <p className="font-semibold md:text-3xl text-2xl md:w-[580px] mt-10">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odit vitae nihil vero sit labore itaque soluta error totam, temporibus illum et incidunt asperiores laborum unde eligendi odio sapiente nemo. Optio.</p>
+                    <p className="font-semibold md:text-3xl text-2xl md:w-[580px] mt-10">
+                    Soy un desarrollador Front-End con tres años de experiencia especializado en la creación y ejecución de proyectos tecnológicos innovadores. Mi pasión radica en desarrollar soluciones eficientes y vanguardistas, respaldadas por mi expertise en tecnologías como React, React Native, Nest, TypeScript y Node.js.
+                    </p>
+                    <p className="font-semibold md:text-3xl text-2xl md:w-[580px] mt-10">
+                    Mi destreza se refleja en mi capacidad para abordar desafíos complejos y convertir los requisitos comerciales en código funcional de alta calidad. Disfruto enfrentando problemas difíciles y colaborando estrechamente con equipos multidisciplinarios para lograr resultados sobresalientes.
+                    </p>
                     <Button
                         className="mt-10 w-[200px] h-[50px]"
                         onClick={handleDescargarCV}
